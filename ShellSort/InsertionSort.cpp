db248a284c80199a4766131a7452507b85fdcd2c
gapm1-ua <gapm1@alu.ua.es>
2025-04-02 06:40:56 +0200

    Rand problems

--- ShellSort/InsertionSort.cpp
+++ ShellSort/InsertionSort.cpp
@@ -29,10 +29,10 @@ vector<int> InsertionSort::generateIntVector(int vecSize)
 
 vector<float> InsertionSort::generateFloatVector(int vecSize)
 {
-	srand(time(NULL));
+	//srand((unsigned)time(NULL));
 	vector<float> vec(vecSize);
 	for (unsigned int i = 0; i < vecSize; i++) {
-		vec[i] = (float)(rand() % vecSize + 1); // * 1.13;
+		vec[i] = (float)(rand() % 10) / (float)(rand() % 10); // * 1.13;
 	}
 
 	return vec;
@@ -85,34 +85,52 @@ void InsertionSort::insertionSortExecutionC()
 
 void InsertionSort::insertionSortExecutionAsm()
 {
+	vector<int> newRandomIntVector;
+	vector<float> newRandomFloatVector;
+	newRandomIntVector = generateIntVector(vecSize);
+	newRandomFloatVector = generateFloatVector(vecSize);
+
+	int* intPtr = &newRandomIntVector[0];
+	float* floatPtr = &randomFloatVector[0];
+	cout << "Vector de enteros sin ordenar (ASM):" << endl;
+	for (int i = 0; i < vecSize; i++) {
+		cout << newRandomIntVector[i] << ", ";
+	}
+	cout << endl;
 	//ORDENAMOS EL VECTOR DE ENTEROS
 	for (int i = 1; i < vecSize; ++i) {
 		__asm {
 			mov ecx, i                 // ecx = i
-			mov eax, randomIntVector  // eax = &randomIntVector[0]
-			mov edx, [eax + ecx * 4]    // edx = key = randomIntVector[i]
+			mov eax, intPtr  // eax = &newRandomIntVector[0]
+			mov edx, [eax + ecx * 4]    // edx = key = newRandomIntVector[i]
 			dec ecx                   // j = i - 1
 
 			loop_start :
 			cmp ecx, -1               // while (j >= 0)
 				jl insert_key
 
-				mov ebx, [eax + ecx * 4]    // ebx = randomIntVector[j]
-				cmp ebx, edx              // if randomIntVector[j] > key
+				mov ebx, [eax + ecx * 4]    // ebx = newRandomIntVector[j]
+				cmp ebx, edx              // if newRandomIntVector[j] > key
 				jle insert_key
 
-				mov[eax + ecx * 4 + 4], ebx // randomIntVector[j + 1] = randomIntVector[j]
+				mov[eax + ecx * 4 + 4], ebx // newRandomIntVector[j + 1] = newRandomIntVector[j]
 				dec ecx
 				jmp loop_start
 
 				insert_key :
-			mov[eax + ecx * 4 + 4], edx // randomIntVector[j + 1] = key
+			mov[eax + ecx * 4 + 4], edx // newRandomIntVector[j + 1] = key
 		}
 	}
 
-	// Mostrar resultados
+	cout << "Vector de enteros ordenado (ASM):" << endl;
 	for (int i = 0; i < vecSize; i++) {
-		cout << randomIntVector[i] << ", ";
+		cout << newRandomIntVector[i] << ", ";
+	}
+	cout << endl;
+
+	cout << "Vector de flotantes sin ordenar (ASM parcial):" << endl;
+	for (int i = 0; i < vecSize; i++) {
+		cout << newRandomFloatVector[i] << ", ";
 	}
 	cout << endl;
 
@@ -122,21 +140,21 @@ void InsertionSort::insertionSortExecutionAsm()
 		int j = i - 1;
 		__asm {
 			mov ecx, i
-			mov eax, randomFloatVector
+			mov eax, floatPtr
 			movss xmm0, [eax + ecx * 4]  // xmm0 = key
 			movss key, xmm0
 		}
 
-		while (j >= 0 && randomFloatVector[j] > key) {
-			randomFloatVector[j + 1] = randomFloatVector[j];
+		while (j >= 0 && newRandomFloatVector[j] > key) {
+			newRandomFloatVector[j + 1] = newRandomFloatVector[j];
 			j--;
 		}
-		randomFloatVector[j + 1] = key;
+		newRandomFloatVector[j + 1] = key;
 	}
 
 	cout << "Vector de flotantes ordenado (ASM parcial):" << endl;
 	for (int i = 0; i < vecSize; i++) {
-		cout << randomFloatVector[i] << ", ";
+		cout << newRandomFloatVector[i] << ", ";
 	}
 	cout << endl;
 }
